888db4f9b2753164d19f6fb2fbd93b985a8dd3f3
Chinh Le <chinh.le11@gmail.com>
2020-03-28 22:04:47 -0400

    Add lint and clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -8,10 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
+          Edit <code>src/App.js</code> and sav
+{' '}
+<code>src/App.js</code>
+{' '}
+and save to reload.
+Name="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
