9cfef899c7ecae48212cdcb41edbd51919889a14
Chinh Le <chinh.le11@gmail.com>
2020-03-30 09:11:46 -0400

    Add first component

---
+++ src/const.js
@@ -0,0 +1,35 @@
+/* import pianoQ from './assets/sounds/pianos/fat_big_techno_kick_and_hat_one_shot_kwu.mp3';
+import pianoW from './assets/sounds/pianos/heavy_techno_kick_silent_servant_like_w38.mp3';
+import pianoE from './assets/sounds/pianos/natural_techno_bongo_kick_yfh.mp3';
+import pianoA from './assets/sounds/pianos/panned_hard_techno_kick_r2x.mp3';
+import pianoS from './assets/sounds/pianos/punchy_clicky_techno_kick_z9w.mp3';
+import pianoD from './assets/sounds/pianos/pushing_steely_techno_kick_mco.mp3';
+import pianoZ from './assets/sounds/pianos/reverbed_dark_rolling_techno_kick_bpw.mp3';
+import pianoX from './assets/sounds/pianos/sloppy_classic_techno_kick_k1m.mp3';
+import pianoC from './assets/sounds/pianos/summer_house_tuned_kick_rwv.mp3';
+ */
+import drumQ from './assets/sounds/drums/fat_big_techno_kick_and_hat_one_shot_kwu.mp3';
+import drumW from './assets/sounds/drums/heavy_techno_kick_silent_servant_like_w38.mp3';
+import drumE from './assets/sounds/drums/natural_techno_bongo_kick_yfh.mp3';
+import drumA from './assets/sounds/drums/panned_hard_techno_kick_r2x.mp3';
+import drumS from './assets/sounds/drums/punchy_clicky_techno_kick_z9w.mp3';
+import drumD from './assets/sounds/drums/pushing_steely_techno_kick_mco.mp3';
+import drumZ from './assets/sounds/drums/reverbed_dark_rolling_techno_kick_bpw.mp3';
+import drumX from './assets/sounds/drums/sloppy_classic_techno_kick_k1m.mp3';
+import drumC from './assets/sounds/drums/summer_house_tuned_kick_rwv.mp3';
+
+export const keys = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
+
+export const drums = new Map([
+  [keys[0], drumQ],
+  [keys[1], drumW],
+  [keys[2], drumE],
+  [keys[3], drumA],
+  [keys[4], drumS],
+  [keys[5], drumD],
+  [keys[6], drumZ],
+  [keys[7], drumX],
+  [keys[8], drumC],
+]);
+
+export const pianos = new Map();
