422aad032b8aefa8a2acbdd7b16d3e36fd6ccb3c
Chinh Le <chinh.le11@gmail.com>
2020-03-31 11:01:15 -0400

    Complete

--- src/components/KeysComponent.js
+++ src/components/KeysComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Button, ButtonGroup } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 import { off } from '../const';
 
--- src/components/PowerComponent.js
+++ src/components/PowerComponent.js
@@ -7,20 +7,21 @@ import { off } from '../const';
 const PowerComponent = (props) => {
   const { power, handler } = { ...props };
   const cssClasses = {
-    className: 'rounded-pill text-uppercase btn__power--size'
+    className: `rounded-pill text-uppercase btn__power--size ${power === off && 'text-secondary'}`
   };
   const cssInline = {
     style: {
       width: '40px',
       height: '30px',
       borderWidth: '2px',
-      backgroundColor: `${power === off ? 'transparent' : ''}`
+      backgroundColor: `${power === off ? 'transparent' : ''}`,
+      fontSize: '0.8rem'
     }
   };
   
   return (
     <Col className="text-right">
-      <Button onClick={handler} variant="danger" {...cssClasses} {...cssInline} />
+      <Button onClick={handler} variant="danger" {...cssClasses} {...cssInline}>{power}</Button>
     </Col>    
   );
 };
