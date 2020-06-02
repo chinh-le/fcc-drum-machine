a2a1fd3f6e06238149472038f40602491919ea4e
Chinh Le <chinh.dev11@gmail.com>
2020-06-02 05:08:21 -0400

    Fix font size and spacing

--- src/components/LabelComponent.js
+++ src/components/LabelComponent.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Col } from 'react-bootstrap';
 
 const LabelComponent = (props) => {
-    const {label} = {...props};
-    
-    return (
-        <Col>
-            <p className="d-flex justify-content-center flex-column rounded-lg border w-100 text-center" style={{height:'40px'}} id="display">{label}</p>
-        </Col>
-    )
+  const { label } = { ...props };
+
+  return (
+    <Col>
+      <p className="d-flex justify-content-center flex-column rounded-lg border w-100 text-center" style={{ height: '40px', fontSize: '0.8rem' }} id="display">{label}</p>
+    </Col>
+  );
 };
 
 export default LabelComponent;
